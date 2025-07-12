ded56a2e467daa3d4ae40e69b3c4a6d6761d2c76
ic005k <zh99889@126.com>
2025-07-13 07:21:49 +0800

    test

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,7 @@ int main(int argc, char* argv[]) {
   showSplash();
 
   // 禁用文本选择（针对所有的可输入的编辑框）
-  qputenv("QT_QPA_NO_TEXT_HANDLES", "1");
+  // qputenv("QT_QPA_NO_TEXT_HANDLES", "1");
 
   // 设置应用程序标识（为QML里面使用Settings做准备）
   app.setOrganizationName("KnotCompany");
